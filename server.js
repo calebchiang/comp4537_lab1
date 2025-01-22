1eb1a201b0453f17319d36c4f9f691d6bfd70a56
Caleb Chiang <calebchiang1002@gmail.com>
2025-01-21 19:04:57 -0800

    updated express

--- server.js
+++ server.js
@@ -7,7 +7,16 @@ const PORT = process.env.PORT || 3000;
 // Serve static files from the "public" directory
 app.use(express.static(path.join(__dirname, "public")));
 
-// Catch-all route to serve "index.html" for any unknown routes
+// Specific routes for individual HTML pages
+app.get("/writer", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "writer.html"));
+});
+
+app.get("/reader", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "reader.html"));
+});
+
+// Catch-all route for other requests (optional)
 app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "public", "index.html"));
 });
